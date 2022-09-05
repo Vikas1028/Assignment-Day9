d2f6b54e00fbdc07a3be96486b544768aba6c4b9
Vikas1028 <vikasbhandekar2018@gmail.com>
2022-09-05 17:16:39 +0530

    Add: Monthly Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,17 +1,39 @@
-a=$((RANDOM%2))
-#echo "Wage per Hour is 20"
-#echo "Full Day Hour is 8"
-WPH=20
-FDH=8
-EDW=$((WPH*FDH))
-#echo "Employee Daily Wage: "$EDW
+a=0
+TotalWage=$a
+b=0
+WorkingDays=$b
+c=0
+Hours=$c
+d=20
+WageperHour=$d
+e=8
+FulldayHours=$e
+f=4
+HalfdayHours=$f
+h=$((d*e))
+FulldayWage=$h
+i=$((d*f))
+HalfDayWage=$i
+j=0
+tatalWorkingDays=$j
 
-#Solve Using Switch case Statement
-case $a in
-0)echo "Employee is Absent"
+while((b!=20 && c<101))
+do
+z=$((RANDOM%3))
+
+case $z in
+0)z=$z
 ;;
-1)echo "Employee is Present"
-  echo "Daily Wage :"$EDW
+1)a=$((a+h))
+  b=$((b+1))
+  c=$((c+8))
+;;
+2)a=$((a+i))
+  b=$((b+1))
+  c=$((c+4))
 esac
 
-echo "Wages for a Month :" $((EDW*20))
+j=$((j+1))
+done
+
+echo "Wage of the Month :" $a
