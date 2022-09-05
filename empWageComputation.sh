fdb37b2c251c04f20aa080545fb836775ea7c5ab
Vikas1028 <vikasbhandekar2018@gmail.com>
2022-09-05 16:04:59 +0530

    Add: Part Time Employee & Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,9 +11,29 @@ echo "Employee is Absent"
 fi
 
 #Add Employee Daily Wage
-echo "Wage per Hour is 20"
-echo "Full Day Hour is 8"
+#echo "Wage per Hour is 20"
+#echo "Full Day Hour is 8"
 WPH=20
 FDH=8
-EDW=$((20*8))
+EDW=$((WPH*FDH))
 echo "Employee Daily Wage: "$EDW
+
+#add Part time employee
+HDH=4
+if((a==1))
+then
+echo "Part Time employee is Present"
+b=$((RANDOM%2))
+if((b==1))
+then
+PTEW=$((WPH*FDH))
+echo  "Part Time Wage :"$PTEW
+else
+PTEW=$((WPH*HDH))
+echo "Part Time Wage :"$PTEW
+fi
+else
+echo "Part Time Employee is Absent"
+fi
+
+
